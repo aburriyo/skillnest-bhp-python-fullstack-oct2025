bf3dab7dfae3564bad32a5cb13f0ce7aee1d2a36
Diego <diego.canalesv@gmail.com>
2025-11-13 20:17:07 -0300

    modificacion archivo test base de datos

--- clases/semana-06/2025-11-13-clase_24/01_ejemplo_pymysql/test_base_de_datos.py
+++ clases/semana-06/2025-11-13-clase_24/01_ejemplo_pymysql/test_base_de_datos.py
@@ -3,5 +3,27 @@ from pprint import pprint
 
 conexion = connectToMySQL("tienda")
 
+query_join = """
+SELECT 
+    usuarios.id,
+    usuarios.nombre,
+    COUNT(pedidos.id) AS cantidad_pedidos,
+    SUM(pedidos.total) AS total_gastado,
+    AVG(pedidos.total) AS promedio_gastado
+FROM usuarios
+JOIN pedidos ON usuarios.id = pedidos.usuario_id
+WHERE pedidos.fecha >= '2024-02-01'
+GROUP BY usuarios.id, usuarios.nombre
+HAVING COUNT(pedidos.id) >= 2;
+"""
+
+
+resultados = conexion.query_db(query_join)
+
+conexion = connectToMySQL("tienda")
+
 resultados = conexion.query_db("SELECT * FROM productos;")
-pprint(resultados)
+
+for registro in resultados:
+    pprint(registro)
+    break
